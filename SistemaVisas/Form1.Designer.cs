feabe5cc7adb955a1d56c0766aaa30c1bde70912
Josue Eguia <eguiajosue9@gmail.com>
2022-05-25 22:07:56 -0500

    Controles dinamicos

--- SistemaVisas/Form1.Designer.cs
+++ SistemaVisas/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dtgVisas = new System.Windows.Forms.DataGridView();
+            this.dtgVisasTrabajo = new System.Windows.Forms.DataGridView();
             this.lstClientes = new System.Windows.Forms.ListBox();
             this.grpDatosCliente = new System.Windows.Forms.GroupBox();
             this.dtpFechaNacimiento = new System.Windows.Forms.DateTimePicker();
@@ -50,38 +50,36 @@
             this.lblNumeroVisa = new System.Windows.Forms.Label();
             this.btnAgregarCliente = new System.Windows.Forms.Button();
             this.btnAgregarVisa = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
-            this.label2 = new System.Windows.Forms.Label();
-            this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.label5 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dtgVisas)).BeginInit();
+            this.grpDatosEspecialesVisa = new System.Windows.Forms.GroupBox();
+            this.dtgVisasTemporales = new System.Windows.Forms.DataGridView();
+            this.dtgVisasEstudiante = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgVisasTrabajo)).BeginInit();
             this.grpDatosCliente.SuspendLayout();
             this.grpDatosVisa.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgVisasTemporales)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgVisasEstudiante)).BeginInit();
             this.SuspendLayout();
             // 
-            // dtgVisas
+            // dtgVisasTrabajo
             // 
-            this.dtgVisas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgVisas.Location = new System.Drawing.Point(184, 259);
-            this.dtgVisas.Name = "dtgVisas";
-            this.dtgVisas.Size = new System.Drawing.Size(604, 179);
-            this.dtgVisas.TabIndex = 0;
+            this.dtgVisasTrabajo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgVisasTrabajo.Location = new System.Drawing.Point(184, 285);
+            this.dtgVisasTrabajo.Name = "dtgVisasTrabajo";
+            this.dtgVisasTrabajo.Size = new System.Drawing.Size(854, 82);
+            this.dtgVisasTrabajo.TabIndex = 0;
             // 
             // lstClientes
             // 
             this.lstClientes.FormattingEnabled = true;
-            this.lstClientes.Location = new System.Drawing.Point(12, 265);
+            this.lstClientes.Location = new System.Drawing.Point(35, 292);
             this.lstClientes.Name = "lstClientes";
-            this.lstClientes.Size = new System.Drawing.Size(166, 173);
+            this.lstClientes.Size = new System.Drawing.Size(124, 277);
             this.lstClientes.TabIndex = 1;
+            this.lstClientes.Click += new System.EventHandler(this.lstClientes_Click);
             // 
             // grpDatosCliente
             // 
@@ -91,7 +89,7 @@
             this.grpDatosCliente.Controls.Add(this.txtNacionalidadCliente);
             this.grpDatosCliente.Controls.Add(this.lblNombre);
             this.grpDatosCliente.Controls.Add(this.txtNombreCliente);
-            this.grpDatosCliente.Location = new System.Drawing.Point(12, 12);
+            this.grpDatosCliente.Location = new System.Drawing.Point(163, 12);
             this.grpDatosCliente.Name = "grpDatosCliente";
             this.grpDatosCliente.Size = new System.Drawing.Size(220, 201);
             this.grpDatosCliente.TabIndex = 2;
@@ -159,7 +157,7 @@
             this.grpDatosVisa.Controls.Add(this.textBox3);
             this.grpDatosVisa.Controls.Add(this.cboTipoVisa);
             this.grpDatosVisa.Controls.Add(this.lblNumeroVisa);
-            this.grpDatosVisa.Location = new System.Drawing.Point(238, 12);
+            this.grpDatosVisa.Location = new System.Drawing.Point(389, 12);
             this.grpDatosVisa.Name = "grpDatosVisa";
             this.grpDatosVisa.Size = new System.Drawing.Size(252, 201);
             this.grpDatosVisa.TabIndex = 3;
@@ -234,11 +232,13 @@
             // 
             // cboTipoVisa
             // 
+            this.cboTipoVisa.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboTipoVisa.FormattingEnabled = true;
             this.cboTipoVisa.Location = new System.Drawing.Point(6, 39);
             this.cboTipoVisa.Name = "cboTipoVisa";
             this.cboTipoVisa.Size = new System.Drawing.Size(200, 21);
             this.cboTipoVisa.TabIndex = 0;
+            this.cboTipoVisa.SelectedValueChanged += new System.EventHandler(this.cboTipoVisa_SelectedValueChanged);
             // 
             // lblNumeroVisa
             // 
@@ -251,7 +251,7 @@
             // 
             // btnAgregarCliente
             // 
-            this.btnAgregarCliente.Location = new System.Drawing.Point(12, 219);
+            this.btnAgregarCliente.Location = new System.Drawing.Point(163, 219);
             this.btnAgregarCliente.Name = "btnAgregarCliente";
             this.btnAgregarCliente.Size = new System.Drawing.Size(220, 34);
             this.btnAgregarCliente.TabIndex = 4;
@@ -261,145 +261,112 @@
             // 
             // btnAgregarVisa
             // 
-            this.btnAgregarVisa.Location = new System.Drawing.Point(238, 219);
+            this.btnAgregarVisa.Location = new System.Drawing.Point(389, 219);
             this.btnAgregarVisa.Name = "btnAgregarVisa";
-            this.btnAgregarVisa.Size = new System.Drawing.Size(252, 34);
+            this.btnAgregarVisa.Size = new System.Drawing.Size(510, 34);
             this.btnAgregarVisa.TabIndex = 5;
             this.btnAgregarVisa.Text = "Agregar Visa";
             this.btnAgregarVisa.UseVisualStyleBackColor = true;
+            this.btnAgregarVisa.Click += new System.EventHandler(this.btnAgregarVisa_Click);
             // 
-            // groupBox1
+            // grpDatosEspecialesVisa
             // 
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.dateTimePicker3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.dateTimePicker4);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.textBox4);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.textBox5);
-            this.groupBox1.Controls.Add(this.comboBox2);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Location = new System.Drawing.Point(496, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(252, 201);
-            this.groupBox1.TabIndex = 13;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Datos de Visado";
+            this.grpDatosEspecialesVisa.Location = new System.Drawing.Point(647, 12);
+            this.grpDatosEspecialesVisa.Name = "grpDatosEspecialesVisa";
+            this.grpDatosEspecialesVisa.Size = new System.Drawing.Size(252, 201);
+            this.grpDatosEspecialesVisa.TabIndex = 13;
+            this.grpDatosEspecialesVisa.TabStop = false;
+            this.grpDatosEspecialesVisa.Text = "Datos de Visado";
             // 
-            // label1
+            // dtgVisasTemporales
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 13);
-            this.label1.TabIndex = 12;
-            this.label1.Text = "Tipo de Visa";
+            this.dtgVisasTemporales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgVisasTemporales.Location = new System.Drawing.Point(184, 386);
+            this.dtgVisasTemporales.Name = "dtgVisasTemporales";
+            this.dtgVisasTemporales.Size = new System.Drawing.Size(854, 82);
+            this.dtgVisasTemporales.TabIndex = 14;
             // 
-            // dateTimePicker3
+            // dtgVisasEstudiante
             // 
-            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker3.Location = new System.Drawing.Point(113, 175);
-            this.dateTimePicker3.Name = "dateTimePicker3";
-            this.dateTimePicker3.Size = new System.Drawing.Size(93, 20);
-            this.dateTimePicker3.TabIndex = 11;
+            this.dtgVisasEstudiante.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgVisasEstudiante.Location = new System.Drawing.Point(184, 487);
+            this.dtgVisasEstudiante.Name = "dtgVisasEstudiante";
+            this.dtgVisasEstudiante.Size = new System.Drawing.Size(854, 82);
+            this.dtgVisasEstudiante.TabIndex = 15;
             // 
-            // label2
+            // label6
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(113, 159);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 13);
-            this.label2.TabIndex = 10;
-            this.label2.Text = "Fecha de Vencimiento";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(181, 266);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Visas de Trabajo";
             // 
-            // dateTimePicker4
+            // label7
             // 
-            this.dateTimePicker4.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker4.Location = new System.Drawing.Point(6, 175);
-            this.dateTimePicker4.Name = "dateTimePicker4";
-            this.dateTimePicker4.Size = new System.Drawing.Size(93, 20);
-            this.dateTimePicker4.TabIndex = 8;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(184, 370);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Visas Temporales";
             // 
-            // label3
+            // label8
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 159);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(94, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Fecha de Registro";
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(184, 471);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Visas de Estudiante";
             // 
-            // textBox4
+            // label9
             // 
-            this.textBox4.Location = new System.Drawing.Point(6, 128);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(200, 20);
-            this.textBox4.TabIndex = 9;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 112);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(69, 13);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Nacionalidad";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(6, 84);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(200, 20);
-            this.textBox5.TabIndex = 7;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(6, 39);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(200, 21);
-            this.comboBox2.TabIndex = 0;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(3, 68);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(82, 13);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "Número de Visa";
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(32, 266);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(84, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Lista de Clientes";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(1063, 590);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dtgVisasEstudiante);
+            this.Controls.Add(this.dtgVisasTemporales);
+            this.Controls.Add(this.grpDatosEspecialesVisa);
             this.Controls.Add(this.btnAgregarVisa);
             this.Controls.Add(this.btnAgregarCliente);
             this.Controls.Add(this.grpDatosVisa);
             this.Controls.Add(this.grpDatosCliente);
             this.Controls.Add(this.lstClientes);
-            this.Controls.Add(this.dtgVisas);
+            this.Controls.Add(this.dtgVisasTrabajo);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sistema de Visas";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dtgVisas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgVisasTrabajo)).EndInit();
             this.grpDatosCliente.ResumeLayout(false);
             this.grpDatosCliente.PerformLayout();
             this.grpDatosVisa.ResumeLayout(false);
             this.grpDatosVisa.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgVisasTemporales)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgVisasEstudiante)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dtgVisas;
+        private System.Windows.Forms.DataGridView dtgVisasTrabajo;
         private System.Windows.Forms.ListBox lstClientes;
         private System.Windows.Forms.GroupBox grpDatosCliente;
         private System.Windows.Forms.GroupBox grpDatosVisa;
@@ -421,17 +388,13 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label lblFechaRegistro;
         private System.Windows.Forms.TextBox txtNacionalidad;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox grpDatosEspecialesVisa;
+        private System.Windows.Forms.DataGridView dtgVisasTemporales;
+        private System.Windows.Forms.DataGridView dtgVisasEstudiante;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- SistemaVisas/Form1.cs
+++ SistemaVisas/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Text;
 using System.Windows.Forms;
 
 namespace SistemaVisas
@@ -14,6 +9,20 @@ namespace SistemaVisas
     {
         private readonly ArchivoSecuencialSerealizadoBinario<Cliente> miArchivo =
             new ArchivoSecuencialSerealizadoBinario<Cliente>("C:\\Datos\\Archivo.dat");
+
+        #region CONTROLES DINAMICOS
+
+        private Label lblNombreEscuela = new Label();
+        private TextBox txtNombreEscuela = new TextBox();
+        private Label lblNombreEmpresa = new Label();
+        private TextBox txtNombreEmpresa = new TextBox();
+        private Label lblFechaLlegada = new Label();
+        private Label lblFechaRegreso = new Label();
+        private DateTimePicker dtmFechaIda = new DateTimePicker();
+        private DateTimePicker dtmFechaRegreso = new DateTimePicker();
+
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +34,30 @@ namespace SistemaVisas
             cboTipoVisa.Items.Add("Visa de estudiante");
             cboTipoVisa.Items.Add("Visa temporal");
 
+            dtgVisasEstudiante.Columns.Add("numeroVisa", "Numero de Visa");
+            dtgVisasEstudiante.Columns.Add("nacionalidad", "Nacionalidad");
+            dtgVisasEstudiante.Columns.Add("fechaRegistro", "Fecha de Registro");
+            dtgVisasEstudiante.Columns.Add("fechaVencimiento", "Fecha de Vencimiento");
+            dtgVisasEstudiante.Columns.Add("precioBase", "Precio Base");
+            dtgVisasEstudiante.Columns.Add("nombreEscuela", "Nombre de la Escuela");
+            dtgVisasEstudiante.Columns.Add("costoTotal", "Costo Total");
+
+            dtgVisasTrabajo.Columns.Add("numeroVisa", "Numero de Visa");
+            dtgVisasTrabajo.Columns.Add("nacionalidad", "Nacionalidad");
+            dtgVisasTrabajo.Columns.Add("fechaRegistro", "Fecha de Registro");
+            dtgVisasTrabajo.Columns.Add("fechaVencimiento", "Fecha de Vencimiento");
+            dtgVisasTrabajo.Columns.Add("precioBase", "Precio Base");
+            dtgVisasTrabajo.Columns.Add("nombreEscuela", "Nombre de la Empresa");
+            dtgVisasTrabajo.Columns.Add("costoTotal", "Costo Total");
+
+            dtgVisasTemporales.Columns.Add("numeroVisa", "Numero de Visa");
+            dtgVisasTemporales.Columns.Add("nacionalidad", "Nacionalidad");
+            dtgVisasTemporales.Columns.Add("fechaRegistro", "Fecha de Registro");
+            dtgVisasTemporales.Columns.Add("fechaVencimiento", "Fecha de Vencimiento");
+            dtgVisasTemporales.Columns.Add("precioBase", "Precio Base");
+            dtgVisasTemporales.Columns.Add("cantidadTiempo", "Cantidad de Meses");
+            dtgVisasTemporales.Columns.Add("costoTotal", "Costo Total");
+
             if (lstClientes.Items.Count <= 0)
                 grpDatosVisa.Enabled = false;
             else grpDatosVisa.Enabled = true;
@@ -34,7 +67,7 @@ namespace SistemaVisas
 
         private void btnAgregarCliente_Click(object sender, EventArgs e)
         {
-            Cliente nuevoCliente = new Cliente();
+            var nuevoCliente = new Cliente();
             try
             {
                 nuevoCliente.Nombre = txtNombreCliente.Text;
@@ -53,7 +86,8 @@ namespace SistemaVisas
             {
                 miArchivo.AbrirEnModoEscritura();
                 miArchivo.GrabarObjeto(nuevoCliente);
-                MessageBox.Show("Cliente agregado exitosamente", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Cliente agregado exitosamente", "Exito", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
             }
             catch (Exception exception)
             {
@@ -68,9 +102,14 @@ namespace SistemaVisas
             MostrarDatos();
         }
 
+        private void btnAgregarVisa_Click(object sender, EventArgs e)
+        {
+            
+        }
+
         private void MostrarDatos()
         {
-            Cliente miCliente = new Cliente();
+            var miCliente = new Cliente();
             lstClientes.Items.Clear();
             try
             {
@@ -84,6 +123,96 @@ namespace SistemaVisas
             catch (Exception e)
             {
                 MessageBox.Show($"{e}");
+            }
+        }
+
+        #region MUESTREO DE DATOS DE LA VISA EN EL DATAGRIDVIEW
+        private void MostrarVisaEstudiante(Cliente cliente)
+        {
+            dtgVisasEstudiante.Rows.Clear();
+            foreach (var clienteListaVisaEstudiante in cliente.ListaVisaEstudiantes)
+            {
+                dtgVisasEstudiante.Rows.Clear();
+            }
+        }
+
+        private void MostrarVisaTrabajo(Cliente cliente)
+        {
+            dtgVisasTrabajo.Rows.Clear();
+            foreach (var clienteListaVisaTrabajo in cliente.ListaVisaTrabajo)
+            {
+                dtgVisasTrabajo.Rows.Clear();
+            }
+        }
+
+        private void MostrarVisaTemporal(Cliente cliente)
+        {
+            dtgVisasTemporales.Rows.Clear();
+            foreach (var clienteListaVisaTemporal in cliente.ListaVisaTemporal)
+            {
+                dtgVisasTemporales.Rows.Clear();
+            }
+        }
+        #endregion
+
+        private void lstClientes_Click(object sender, EventArgs e)
+        {
+            grpDatosVisa.Enabled = true;
+            Cliente clienteSeleccionado = new Cliente();
+            clienteSeleccionado = (Cliente)lstClientes.SelectedItem;
+
+            if (clienteSeleccionado == null)
+            {
+                MessageBox.Show("Debe de seleccionar un cliente", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            MostrarVisaEstudiante(clienteSeleccionado);
+            MostrarVisaTrabajo(clienteSeleccionado);
+            MostrarVisaTemporal(clienteSeleccionado);
+        }
+
+        private void cboTipoVisa_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cboTipoVisa.Text == "Visa de estudiante")
+            {
+                grpDatosEspecialesVisa.Controls.Clear();
+
+                lblNombreEscuela.Text = "Escuela:";
+                lblNombreEscuela.Location = new System.Drawing.Point(10, 30);
+                txtNombreEscuela.Location = new System.Drawing.Point(10, 55);
+
+                grpDatosEspecialesVisa.Controls.Add(lblNombreEscuela);
+                grpDatosEspecialesVisa.Controls.Add(txtNombreEscuela);
+            }
+
+            if (cboTipoVisa.Text == "Visa de trabajo")
+            {
+                grpDatosEspecialesVisa.Controls.Clear();
+
+                lblNombreEmpresa.Text = "Empresa:";
+                lblNombreEmpresa.Location = new System.Drawing.Point(10, 30);
+                txtNombreEmpresa.Location = new System.Drawing.Point(10, 55);
+
+                grpDatosEspecialesVisa.Controls.Add(lblNombreEmpresa);
+                grpDatosEspecialesVisa.Controls.Add(txtNombreEmpresa);
+            }
+
+            if (cboTipoVisa.Text == "Visa temporal")
+            {
+                grpDatosEspecialesVisa.Controls.Clear();
+
+                lblFechaLlegada.Text = "Llegada:";
+                lblFechaLlegada.Location = new System.Drawing.Point(10, 30);
+                dtmFechaIda.Location = new System.Drawing.Point(10, 55);
+
+                lblFechaRegreso.Text = "Regreso:";
+                lblFechaRegreso.Location = new System.Drawing.Point(10, 90);
+                dtmFechaRegreso.Location = new System.Drawing.Point(10, 115);
+
+                grpDatosEspecialesVisa.Controls.Add(lblFechaLlegada);
+                grpDatosEspecialesVisa.Controls.Add(lblFechaRegreso);
+                grpDatosEspecialesVisa.Controls.Add(dtmFechaIda);
+                grpDatosEspecialesVisa.Controls.Add(dtmFechaRegreso);
             }
         }
     }
